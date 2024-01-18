eea872b6c15d104e6cd97b4362a99b72300e600d
Elijah Kelly <ek16@icsd.k12.ny.us>
2024-01-17 20:49:30 -0500

    Added 2 new methods for finding note distance, started testing

--- note_detection/pos_math.py
+++ note_detection/pos_math.py
@@ -8,26 +8,47 @@ with open(os.path.join("note_detection", "constants.yml"), "r") as fp:
     params = safe_load(fp)
 
 def computeEllipseAnglesFromCam(ellipses):
-    """Assuming the inputted ellipses are circles viewed from an angle, calculates the angle from that circle to the camera"""
+    """Assuming the inputted ellipses are circles viewed from an angle, calculates the angle from that circle to the camera."""
     angles = []
     for ellipse in ellipses:
         major = ellipse[1][1]
         minor = ellipse[1][0]
         angle = ellipse[2] # The clockwise angle from a horizontal line to the minor axis
-        radiansPerPixelHeight = math.radians(params["FOV_HEIGHT_DEGREES"] / params["FOV_HEIGHT_PIX"])
-        radiansPerPixelWidth = math.radians(params["FOV_WIDTH_DEGREES"] / params["FOV_WIDTH_PIX"])
-        ellipseAngleHeight = minor * math.sin(math.radians(angle)) * radiansPerPixelHeight
-        ellipseAngleWidth = minor * abs(math.cos(math.radians(angle))) * radiansPerPixelWidth
-        ellipseAngle = math.sqrt(ellipseAngleHeight ** 2 + ellipseAngleWidth ** 2)
+        ellipseAngle = f.pixelsToRadians(minor, angle)
         minorLengthRelativeToDiameter = minor / major
         angleFromCam = (math.pi - ellipseAngle) / 2 - math.asin(minorLengthRelativeToDiameter * math.sin((math.pi + ellipseAngle) / 2))
         angles.append(angleFromCam)
     return angles
 
-def computeNoteDistancesFromCam(angles):
-    """Uses the angle of a note from the camera to calculate its distance, assuming it is on the floor"""
+def computeNoteDistancesFromAngles(angles):
+    """Uses the angle of a note from the camera to calculate its distance, assuming it is on the floor."""
     distances = []
     for angle in angles:
         distance = params["CAMERA_HEIGHT_IN"] * math.tan(angle)
+        distances.append(distance)
+    return distances
+
+def computeNoteDistancesFromMajorAxes(ellipses):
+    """Uses the major axis of a note to calculate its distance, assuming it is on the floor."""
+    distances = []
+    for ellipse in ellipses:
+        major = ellipse[1][1]
+        angle = ellipse[2]
+        ellipseAngle = f.pixelsToRadians(major, angle - 90)
+        distanceStraightOn = 6 / math.tan(ellipseAngle / 2) # tan(ellipseAngle) SHOULD not be 0 or infinity
+        if distanceStraightOn < params["CAMERA_HEIGHT_IN"]: distance = 0
+        else: distance = math.sqrt(distanceStraightOn ** 2 - params["CAMERA_HEIGHT_IN"] ** 2)
+        distances.append(distance)
+    return distances
+
+def computeNoteDistancesFromCenters(centers):
+    """Uses the center point of a note to calculate its distance based on the tilt of the camera, assuming it is on the floor and the center point is below the camera"""
+    distances = []
+    for center in centers:
+        centerY = center[1]
+        heightAboveCamCenter = params["FOV_HEIGHT_PIX"] / 2 - centerY
+        angleAboveCamCenter = f.pixelsToRadians(heightAboveCamCenter, 90)
+        angleAboveHorizontal = angleAboveCamCenter + math.radians(params["CAMERA_CENTER_ANGLE_DEGREES"])
+        distance = params["CAMERA_HEIGHT_IN"] / math.tan(angleAboveHorizontal)
         distances.append(distance)
     return distances
